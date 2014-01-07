e8e867f539cd451eb7dbadce4e322de56c1f043c
Chris Meadows <meadoch1@gmail.com>
2014-01-07 11:34:56 -0500

    Added a junk secret_token.rb for start up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularSpike::Application.config.secret_token = '6d4bf5d106f2c42f757c139c2f312be5c529e9e1fa4e63a06688db82a204069d18bc8d17215e8dde7da1b012993d72e25589ebde223f91181be7d06d053e4391'
